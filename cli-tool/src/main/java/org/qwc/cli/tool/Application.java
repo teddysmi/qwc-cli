84a1d9fa01dd822a16006b859636f1451dae241b
Ee King Beh <kingzz_90@live.co.uk>
2020-12-14 22:31:31 +0800

    master update

--- cli-tool/src/main/java/org/qwc/cli/tool/Application.java
+++ cli-tool/src/main/java/org/qwc/cli/tool/Application.java
@@ -14,6 +14,8 @@ import org.springframework.shell.jline.PromptProvider;
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
+
+System.out.println("Hello World");
 		SpringApplication.run(Application.class, args);
 	}
 
